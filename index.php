96ac269ad5858a2987228140a1ae0fbed05b86ac
marco <dobermannpoker86@gmail.com>
2020-05-11 15:57:14 +0200

    var fixato nel js

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <!-- jquery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <!--  -->
         <script src="https://cdn.jsdelivr.net/npm/chart.js@2/dist/Chart.min.js"></script>
         <!-- css -->
@@ -14,14 +14,12 @@
     </head>
 
     <body>
-    
 
         <div class="container">
             <div class="row">
                 <div class="col">
 
                     <canvas id="line" ></canvas>
-
 
                 </div>
 
--- my.js
+++ my.js
@@ -7,70 +7,79 @@ function printLineChartDemo() {
     $.ajax({
         url: "server.php",
         method: "GET",
-        success : function(pluto) {
+        success : function(dati) {
 
-            var punti = pluto.data;
-            var mesi = pluto.labels;
+            var punti = dati.data;
+            var mesi = dati.labels;
             console.log(mesi);
             console.log(punti);
+            // ----
 
+            console.log("ho i dati");
+
+            console.log("uso i dati");
+
+            var ctx = $("#line");
+
+            var myChart = new Chart(ctx, {
+
+            type: 'line',
+
+            data: {
+                labels: mesi,
+                datasets: [{
+                    label: '# VENDITE',
+                    data: punti,
+                    backgroundColor: [
+                        'rgba(4, 51, 255, 1)',
+                            'rgba(82, 40, 204, 1)',
+                            'rgba(4, 51, 185, 1)',
+                            'rgba(0, 146, 146, 1)',
+                            'rgba(0, 249, 0, 1)',
+                            'rgba(202, 250, 0, 1)',
+                            'rgba(255, 251, 0, 1)',
+                            'rgba(255, 199, 0, 1)',
+                            'rgba(255, 147, 0, 1)',
+                            'rgba(255, 80, 0, 1)',
+                            'rgba(255, 38, 0, 1)',
+                            'rgba(216, 34, 83, 1)'
+                    ],
+                    borderColor: [
+                        'rgba(4, 51, 255, 1)',
+                            'rgba(82, 40, 204, 1)',
+                            'rgba(4, 51, 185, 1)',
+                            'rgba(0, 146, 146, 1)',
+                            'rgba(0, 249, 0, 1)',
+                            'rgba(202, 250, 0, 1)',
+                            'rgba(255, 251, 0, 1)',
+                            'rgba(255, 199, 0, 1)',
+                            'rgba(255, 147, 0, 1)',
+                            'rgba(255, 80, 0, 1)',
+                            'rgba(255, 38, 0, 1)',
+                            'rgba(216, 34, 83, 1)'
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+            });
+
+        },
+        error : function(){
+            alert("alert!!!");
         }
 
     });
 
-    var ctx = $("#line");
 
-    var myChart = new Chart(ctx, {
-
-    type: 'line',
-
-    data: {
-        labels: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
-        datasets: [{
-            label: '# VENDITE',
-            data: [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322],
-
-            backgroundColor: [
-                'rgba(150, 33, 146, 1)',
-                    'rgba(82, 40, 204, 1)',
-                    'rgba(4, 51, 255, 1)',
-                    'rgba(0, 146, 146, 1)',
-                    'rgba(0, 249, 0, 1)',
-                    'rgba(202, 250, 0, 1)',
-                    'rgba(255, 251, 0, 1)',
-                    'rgba(255, 199, 0, 1)',
-                    'rgba(255, 147, 0, 1)',
-                    'rgba(255, 80, 0, 1)',
-                    'rgba(255, 38, 0, 1)',
-                    'rgba(216, 34, 83, 1)'
-            ],
-            borderColor: [
-                'rgba(150, 33, 146, 1)',
-                    'rgba(82, 40, 204, 1)',
-                    'rgba(4, 51, 255, 1)',
-                    'rgba(0, 146, 146, 1)',
-                    'rgba(0, 249, 0, 1)',
-                    'rgba(202, 250, 0, 1)',
-                    'rgba(255, 251, 0, 1)',
-                    'rgba(255, 199, 0, 1)',
-                    'rgba(255, 147, 0, 1)',
-                    'rgba(255, 80, 0, 1)',
-                    'rgba(255, 38, 0, 1)',
-                    'rgba(216, 34, 83, 1)'
-            ],
-            borderWidth: 3
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
 
 }
 
