4c659bb0a0ed2dbaa877fca623e9091013e47a47
marco <dobermannpoker86@gmail.com>
2020-05-11 12:41:45 +0200

    prima parte ok

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <!-- jquery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!--  -->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2/dist/Chart.min.js"></script>
+        <!-- css -->
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+        <title>chart demo</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                    <canvas id="line" ></canvas>
+
+
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+
+    <script src="my.js" charset="utf-8"></script>
+
+    </body>
+</html>
